c9594026a7fc6cc3a1f73d4c4f0e1c59dd95c74b
rk1022aus <rkomiya@students.cs.ubc.ca>
2026-02-07 14:58:04 -0800

    Work on ResultPage and frontend updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useState } from "react";
+import { StandaloneSignIn } from "./StandaloneSignIn";
 
 function App() {
   return (
